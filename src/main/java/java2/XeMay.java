f6de3be99b80a738d2e3f0fed59178af694d8487
duonghud <chiuroi2312@gmail.com>
2026-03-03 14:41:21 +0700

    Buoi5

---
+++ src/main/java/java2/XeMay.java
@@ -0,0 +1,11 @@
+package java2.buoi5.BaiTap;
+
+public class XeMay {
+    private String name;
+    private double MaLuc;
+
+    public XeMay(String name, double maLuc) {
+        this.name = name;
+        MaLuc = maLuc;
+    }
+}
