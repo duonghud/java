da74facde469e8aa69ec167f342f445f76084daa
any <Duong.BKC16230@student.bkacad.edu.vn>
2026-03-17 17:11:25 +0700

    update1

--- src/main/java/Open/ClosedPrinciple/Vidu4/App.java
+++ src/main/java/Open/ClosedPrinciple/Vidu4/App.java
@@ -5,24 +5,24 @@ import java.util.List;
 public class App {
     public static void main(String[] args) {
 
-        // 1. Gửi email
+        /// 1. Gửi email
         NotificationSender emailSender = new EmailSender();
         NotificationService emailService = new NotificationService(emailSender);
-        emailService.notify("Hello Email", "user@example.com");
+        emailService.notify("Chào mừng bạn đã trúng tuyển vào công ty chung tôi ở vị trí senior. Bạn sẽ bắt đầu đi làm vào ngày mai chúc bạn may mắn", "sepCTCP@KTH.com");
 
         System.out.println("------");
 
-        // 2. Gửi SMS
+        /// 2. Gửi SMS
         NotificationSender smsSender = new SmsSender();
         NotificationService smsService = new NotificationService(smsSender);
-        smsService.notify("Hello SMS", "0123456789");
+        smsService.notify("Bạn đã chúng một chiếc iphone 16Promax chuyển cho chúng tôi 5.000.000 để nhận thưởng", "0287692544");
 
         System.out.println("------");
 
-        // 3. Gửi Push Notification
+        /// 3. Gửi Push Notification
         NotificationSender pushSender = new PushSender();
         NotificationService pushService = new NotificationService(pushSender);
-        pushService.notify("Hello Push", "device_token_123");
+        pushService.notify("Lỗi hệ thống máy chủ", "device tại Hà Nội");
 
     }
 }
